1dcc19abd7b3f3f4529db82b15261034ab871d8e
Mateusz Derks <mateusz.derks@toptal.com>
2016-11-26 12:38:15 +0100

    Remember url

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {run} from '@cycle/xstream-run'
 import {div, label, input, button, hr, h1, makeDOMDriver} from '@cycle/dom'
+import storageDriver from '@cycle/storage'
 import xs from 'xstream'
 import debounce from 'xstream/extra/debounce'
 
@@ -7,6 +8,12 @@ import Badge from './components/badge'
 import BadgeEditor from './components/badge_editor'
 
 function main(sources) {
+  const storedUrl$ = sources.storage.local
+    .getItem('url')
+    .startWith('')
+
+  const url$ = sources.DOM.select('.url').events('input').map(e => e.target.value)
+
   const badgeEditor = BadgeEditor({
     DOM: sources.DOM,
     props$: xs.of({
@@ -20,10 +27,12 @@ function main(sources) {
     props$: badgeEditor.config$.compose(debounce(100))
   })
 
-  const vtree$ = xs.combine(badge.DOM, badgeEditor.DOM).map(([badgeDOM, editorDOM]) => {
+
+
+  const vtree$ = xs.combine(storedUrl$, badge.DOM, badgeEditor.DOM).map(([storedUrl, badgeDOM, editorDOM]) => {
     return div([
       label('Url:'),
-      input('.url', ''),
+      input('.url', { props: { value: storedUrl}}),
       hr(),
       label('ReadKey:'),
       input('.read-key'),
@@ -40,11 +49,22 @@ function main(sources) {
     ])
   })
 
+  const storageRequests$ = url$.map(url => ({
+    key: 'url',
+    value: url
+  }))
+
   const sinks = {
     DOM: vtree$,
+    storage: storageRequests$
   }
 
   return sinks
 }
 
-run(main, { DOM: makeDOMDriver('#root') })
+const drivers = {
+  DOM: makeDOMDriver('#root'),
+  storage: storageDriver
+}
+
+run(main, drivers)
